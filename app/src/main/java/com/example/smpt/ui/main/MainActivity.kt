d9acfb0eae973d96da05d391f769306f99ff5e48
Miron Smardzewski <mironsmardzewski@gmail.com>
2021-09-03 13:28:04 +0200

    added button to main Activity

--- app/src/main/java/com/example/smpt/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/smpt/ui/main/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.smpt.ui.main
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.example.smpt.R
@@ -11,5 +13,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.showTargetsButton).setOnClickListener {
+            Toast.makeText(this, "Troops has been shown", Toast.LENGTH_LONG).show()
+        }
     }
 }
