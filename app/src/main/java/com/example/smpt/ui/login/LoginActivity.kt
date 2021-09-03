99db93412c157ffaa45ad8cdeda6672aa8c62d3e
Marchewka <dominikmarchewka1997@gmail>
2021-09-03 14:35:10 +0200

    ekran logowania

--- app/src/main/java/com/example/smpt/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/smpt/ui/login/LoginActivity.kt
@@ -1,15 +1,24 @@
 package com.example.smpt.ui.login
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.example.smpt.R
+import com.example.smpt.databinding.ActivityLoginBinding
+import com.example.smpt.ui.main.MainActivity
 
 class LoginActivity : AppCompatActivity() {
     private val viewModel: LoginViewModel by viewModels()
+    private val binding: ActivityLoginBinding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 }
