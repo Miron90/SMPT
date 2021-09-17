e7da76929476f0b59d856d21cf992ce7a6b378db
Miron Smardzewski <mironsmardzewski@gmail.com>
2021-09-17 12:28:22 +0200

    fixed and added ssl

--- app/src/main/java/com/example/smpt/ui/Constants.kt
+++ app/src/main/java/com/example/smpt/ui/Constants.kt
@@ -2,8 +2,8 @@ package com.example.smpt.ui
 
 class Constants {
     val USERNAME = "USERNAME"
-    val TILE_URL = "http://172.16.35.152:5001/api/tile/"
-    val BASE_URL = "http://172.16.35.152:5001"
+    val TILE_URL = "https://172.16.35.152:5001/api/tile/"
+    val BASE_URL = "https://172.16.35.152:5001"
     val LOCATION_REQUEST_INTERVAL = 1000
     val LOCATION_REQUEST_FASTESTINTERVAL = 1000
     val LOCATION_REQUEST_MAXWAITTIME = 1000
